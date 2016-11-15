f897479bde73cf029f7631b742f560cc573fe262
vicktorjunior <vicktor.junior@gmail.com>
2016-11-15 00:01:43 -0200

    upload arquivos bootstrap,jquery e plugins

--- IFTCC-NOLL/src/main/resources/static/js/bootstrap.js
+++ IFTCC-NOLL/src/main/resources/static/js/bootstrap.js
@@ -2375,3 +2375,15 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+var btAbrirModal = $("#btAbrirModal");
+var modal = $("#modal");
+var modalClose = $("#modal .modal-close");
+var modalBackground = $("#modal .modal-bg");
+
+btAbrirModal.click(function () {
+    modal.fadeIn(500);
+});
+
+modalClose.click(function () {
+    modal.fadeOut(500);
+});
